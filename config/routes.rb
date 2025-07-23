d86ea82fa8a88f40ac3ef1ee149012a5d759e8bb
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2025-07-23 14:01:21 +0000

    [Create]login signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+  devise_for :users
+  root to: 'homes#top'
+  get 'about' => 'homes#about', as: 'about'
 
 end
