644ad52b3493a06870a3f936533c1c6a25e74356
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2025-08-28 11:52:57 +0000

    Re

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,8 @@ class SearchesController < ApplicationController
     @model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
-		if @model == 'user'
-			@records = User.search_for(@content, @method)
+		if @model == 'admin'
+			@records = Admin.search_for(@content, @method)
 		else
 			@records = Post.search_for(@content, @method)
 		end
