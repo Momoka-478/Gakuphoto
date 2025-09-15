fb14e0ff633a2a713a518acfa8094ea03a2b183a
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2025-09-15 09:47:35 +0000

    [Create]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+maki = User.find_or_create_by!(email: "maki@example.com") do |user|
+  user.name = "Maki"
+  user.password = "password"
+end
+
+ichiro = User.find_or_create_by!(email: "ichiro@example.com") do |user|
+  user.name = "Ichiro"
+  user.password = "password"
+end
+
+momoka = Admin.find_or_create_by!(email: "momoka@example.com") do |admin|
+  admin.name = "Momoka"
+  admin.password = "pass1234"
+end
+
+Post.find_or_create_by!(title: "教室内") do |post_image|
+  post_image.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post_image.body = "きれいに掃除をしました。"
+  post_image.admin = momoka
+end
