ab1ac6dc390e8bc2b01a7dffc4eb5d1f4d85ac1a
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2026-01-07 15:10:03 +0000

    [Update]バリデーションメッセージ

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -3,11 +3,13 @@ class Admin::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_admin.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    if comment.save
-      redirect_to admin_post_path(post)
+    @post = Post.find(params[:post_id])
+    @post_comment = current_admin.post_comments.new(post_comment_params)
+    @post_comment.post = @post
+    if @post_comment.save
+      redirect_to admin_post_path(@post)
+    else
+      render 'admin/posts/show'
     end
   end
 
